42f7d40c0d7fd44c92044bebbba55158606246e8
Nami5011 <maron.011821@gmail.com>
2024-09-23 00:01:34 +0900

    footer link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer>
-      <p class="contact-me">If you'd like to learn more, <a href="https://www.instagram.com/mimahna/">let’s chat</a> or find me on <a href="https://www.instagram.com/mimahna/">Instagram</a>.</p>
+      <p class="contact-me">If you'd like to learn more, <a href="https://www.instagram.com/mimahna/" target="_blank">let’s chat</a> or find me on <a href="https://www.instagram.com/mimahna/" target="_blank">Instagram</a>.</p>
     </footer>
     <?php wp_footer(); ?>
   </body>
